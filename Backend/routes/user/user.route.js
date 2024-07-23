3dbc9bfaf65c5e84178e3215e4c3f4614bccfce9
Jitesh <jiteshsinghshekhawat@gmail.com>
2024-07-23 07:12:41 +0530

    added Route for Users

--- Backend/routes/user/user.route.js
+++ Backend/routes/user/user.route.js
@@ -3,6 +3,7 @@ import registerRoute from "./register.route.js";
 import loginRoute from "./login.route.js"; 
 import forgetPasswordRouter from "./forgetPassword.js"; 
 import resetPasswrodRouter from "./resetPassword.js"; 
+import updatePasswordRouter from "./updatePassword.js"; 
 import jwtValid from "../../middlewares/verifyJwt.middleware.js"; 
 import profileRoute from "./profile.route.js"; 
 
@@ -15,6 +16,8 @@ router.use("/forgotPassword",forgetPasswordRouter);
 router.use("/resetPassword",resetPasswrodRouter); 
 
 router.use('/login',loginRoute); 
+
+router.use('/updatePassword',jwtValid,updatePasswordRouter); 
 
 router.use('/profile',jwtValid,profileRoute); 
 export default router ; 
